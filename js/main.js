c94f495b37dfb3907e1c9770e2c1572b91587b1c
kyrylo_kundik <goliathusua@gmail.com>
2019-09-10 14:48:45 +0300

    fixed main.js, fixed smooth scroll problem with anchor

--- js/main.js
+++ js/main.js
@@ -1,34 +1,66 @@
-window.onscroll = function () {
-	myFunction()
-};
+$(document).ready(function () {
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-var modalAskQuestion = document.getElementById("modalAskQuestion");
-var btnAsk = document.getElementById("btnAsk");
-var span = document.getElementsByClassName("close")[0];
+	window.onscroll = function () {
+		myFunction()
+	};
 
-function myFunction() {
-	if (window.pageYOffset > sticky) {
-		header.classList.add("sticky");
-	} else {
-		header.classList.remove("sticky");
+	var header = document.getElementById("header");
+	var sticky = header.offsetTop;
+	var modalAskQuestion = document.getElementById("modalAskQuestion");
+	var btnAsk = document.getElementById("btnAsk");
+	var span = document.getElementsByClassName("closeBut")[0];
+
+	function myFunction() {
+		if (window.pageYOffset > sticky) {
+			header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
 	}
-}
 
-// When the user clicks the button, open the modalAskQuestion
-btnAsk.onclick = function() {
-    modalAskQuestion.style.display = "block";
-}
+	// When the user clicks the button, open the modalAskQuestion
+	btnAsk.onclick = function () {
+		modalAskQuestion.style.display = "block";
+	};
 
 // When the user clicks on <span> (x), close the modalAskQuestion
-span.onclick = function() {
-    modalAskQuestion.style.display = "none";
-}
+	span.onclick = function () {
+		modalAskQuestion.style.display = "none";
+	};
 
 // When the user clicks anywhere outside of the modalAskQuestion, close it
-window.onclick = function(event) {
-    if (event.target == modalAskQuestion) {
-        modalAskQuestion.style.display = "none";
-    }
-}
+	window.onclick = function (event) {
+		if (event.target === modalAskQuestion) {
+			modalAskQuestion.style.display = "none";
+		}
+	};
+
+	// Add smooth scrolling to all links
+	$('a').on('click', function (event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+			event.stopImmediatePropagation();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - 170
+			}, 1100, function () {
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top - 170
+				}, 0, function () {
+
+				})
+			});
+		} // End if
+	});
+
+	myFunction()
+});
