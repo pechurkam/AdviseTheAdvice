727f5678d72e98513261a6b7df6285c255f30c84
kyrylo_kundik <goliathusua@gmail.com>
2019-09-10 09:25:59 +0300

    done: service and parents sections

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-window.onscroll = function () {
-	myFunction()
-};
+$(document).ready(function () {
+	window.onscroll = function () {
+		myFunction()
+	};
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
+	var header = document.getElementById("header");
+	var sticky = header.offsetTop;
 
-function myFunction() {
-	if (window.pageYOffset > sticky) {
-		header.classList.add("sticky");
-	} else {
-		header.classList.remove("sticky");
+	function myFunction() {
+		if (window.pageYOffset > sticky) {
+			header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
 	}
-}
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - 170
+			}, 1100, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+
+	myFunction()
+});
