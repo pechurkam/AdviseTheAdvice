fa38f3f88fbf6cebae41a06a987c13d5ed7a99ad
kyrylo_kundik <goliathusua@gmail.com>
2019-09-23 13:12:22 +0300

    enabled gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 //найти scss сделать конвертацию и сконвертированный файл положить в директорию
 function style(){
     return gulp.src('./scss/**/*.scss')
@@ -18,8 +19,11 @@ function build() {
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
     gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(concat('style.css'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css'));
     gulp.src('./index.html')
         .pipe(gulp.dest('./dist'));
@@ -39,14 +43,14 @@ function watch(){
     gulp.watch('./*.html').on('change', browserSync.reload)
 }
 
-gulp.task('default', function () {
-    return gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'));
-});
+// gulp.task('default', function () {
+//     return gulp.src('src/app.css')
+//         .pipe(autoprefixer({
+//             browsers: ['last 2 versions'],
+//             cascade: false
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
 
 exports.style = style;
 exports.watch = watch;
